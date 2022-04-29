a50c3bd662309ff41613a424dd40ca266e34b505
Roberto Courech <betosmith2000@hotmail.com>
2022-04-29 08:57:30 -0500

    final

--- src/index.js
+++ src/index.js
@@ -5,15 +5,28 @@ import ReactDOM from 'react-dom';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './index.css';
 
+import Play from './pages/play';
+import Guide from './pages/guide';
+import Leaderboard from './pages/leaderboard';
+
+
+import { getChainOptions, WalletProvider } from '@terra-money/wallet-provider'; 
+
+require('dotenv').config()
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
+ getChainOptions().then(chainOptions => {
 ReactDOM.render(
   <React.StrictMode>
+    <WalletProvider {...chainOptions}>
       <div className="App-header">
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<App />} />
+            <Route path="/play" element={<Play />} />
+              <Route path="/leaderboard" element={<Leaderboard />} />
+               <Route path="/guide" element={<Guide />} /> 
           </Routes>
         </BrowserRouter>
 
@@ -31,7 +44,8 @@ ReactDOM.render(
           >{`Made with @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
-
+  </WalletProvider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+  );
+ });
